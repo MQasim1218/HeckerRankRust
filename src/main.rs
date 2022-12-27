2bc88ffe5cc2ee5195aa6ee2265e007e1dc152b7
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-12-27 22:38:17 +0500

    123

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    let (tot_x, tot_y) = (0, 0);
+    for i in 0..arr.len() {
+        
+    }
+    tot_x - tot_y
+}
+
 fn main() {
     println!("Hello, world!");
+    let arr = vec![vec![1, 2, 3], vec![4, 5, 6], vec![9, 8, 9]];
+    let res = diagonalDifference(&arr);
 }
